ea6ccf3c2b8e961d8c7bac7d8b2b3d4ffe109ceb
candyzp <166943815+candyzp@users.noreply.github.com>
2026-08-30 17:11:51 -0400

    Cache floating shortcut hot-path data

--- src/GUI/FloatingButton/ModuleShortcutButton.hpp
+++ src/GUI/FloatingButton/ModuleShortcutButton.hpp
@@ -12,6 +12,14 @@ class ModuleShortcutButton : public FloatingUIButton
         std::string bgOffSpr = "geode.loader/baseCircle_Medium_Gray.png";
         std::string bgOnSpr = "geode.loader/baseCircle_Medium_Green.png";
 
+        // Shortcut configuration is immutable for the lifetime of this button.
+        // Module::setShortcutConfig recreates the node when configuration changes,
+        // so keeping a local copy avoids rebuilding/copying it every frame.
+        ModuleShortcutConfig shortcutConfig = {};
+        std::string colourChannel = "";
+        std::string shortcutPosXKey = "";
+        std::string shortcutPosYKey = "";
+
     public:
         static ModuleShortcutButton* create(Module* module);
 
